1670078b19a278456fd30edbe01de2b1062f741f
ButterCroissant\steviegiovanni <steviegiovanni@gmail.com>
2018-05-20 08:38:20 +0800

    - finished rigging - tried 3D object sorting layer

---
+++ Assets/Scripts/SortingLayer3D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayer3D : MonoBehaviour {
+	public string sortingLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+		MeshRenderer meshRenderer = GetComponent<MeshRenderer> ();
+		if (meshRenderer) {
+			Debug.Log ("reached");
+			meshRenderer.sortingLayerName = sortingLayer;
+			meshRenderer.sortingOrder = -99;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
